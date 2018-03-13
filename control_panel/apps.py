a754e7a8736073801b28453b165bb9078a1890cb
Borys Lemeshko <lemeshkob@gmail.com>
2018-03-13 10:39:52 +0200

    removed all data

--- control_panel/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ControlPanelConfig(AppConfig):
-    name = 'control_panel'
-    verbose_name = 'Ключові таблиці'
--- control_panel/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render_to_response, render
-
-
-def index_render(request):
-    return render_to_response('control_panel/index.html')
