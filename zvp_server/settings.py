809fedbe7a632741669611c5e95ff83209116471
Anton Yurash <y.anton.ua@gmail.com>
2018-04-10 10:39:59 +0300

    Add main page template

--- zvp_server/settings.py
+++ zvp_server/settings.py
@@ -55,6 +55,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'zvp_server.urls'
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -120,8 +126,3 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-STATIC_URL = '/static/'
