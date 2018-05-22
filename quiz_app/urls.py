e2196a35f08bc79de218e7053c94edaf90f4c0f1
yourash <y.anton.ua@gmail.com>
2018-05-22 12:09:30 +0300

    Add questions base

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^tests', views.render_tests_page, name='tests'),
     url(r'^results', views.render_results, name='results'),
     url(r'^questions', views.render_question_list, name='questions'),
+    url(r'^start-test', views.start_test, name='test'),
 ]
--- quiz_app/views.py
+++ quiz_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .models import *
 from django.contrib.auth.decorators import login_required
+from datetime import datetime, timedelta
 
 
 @login_required
 def render_question_list(request):
     question_list = Question.objects.all()
-    return render(request, 'quiz_app/question.html', {'question_list': question_list})
+    return render(request, 'quiz_app/questions.html', {'question_list': question_list})
 
 
 @login_required
@@ -18,3 +19,16 @@ def render_tests_page(request):
 @login_required
 def render_results(request):
     return render(request, 'quiz_app/results.html')
+
+
+@login_required
+def start_test(request):
+    quiz_minutes = 20
+    questions = Question.objects.all()
+    test_name = 'Тест з ЗВП 1 на тему АК-74'
+    time_start = datetime.now()
+    time_end = time_start + timedelta(minutes=quiz_minutes)
+    return render(request, 'quiz_app/questions.html',
+                  {'questions': questions,
+                   'test_name': test_name,
+                   'time_end':time_end})
